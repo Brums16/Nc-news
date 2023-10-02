bed7b9f5ebf30ce8f53135d7493523ce73afd9e6
Brums16 <chrisbrummitt16@hotmail.co.uk>
2023-10-02 14:29:39 +0100

    task 3 get api endpoints

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const data = require("../db/data/test-data");
 const { app } = require("../app.js");
 const seed = require("../db/seeds/seed");
 const db = require("../db/connection");
+const { fetchEndpoints } = require("../models/topics-model");
 
 beforeEach(() => seed(data));
 afterAll(() => db.end());
@@ -35,6 +36,71 @@ describe("GET /api/topics", () => {
           { slug: "paper", description: "what books are made of" },
         ];
         expect(topics).toEqual(expected);
+      });
+  });
+});
+
+describe("GET /api", () => {
+  test("responds with status code 200", () => {
+    return request(app).get("/api").expect(200);
+  });
+  test("responds with an object", () => {
+    return request(app)
+      .get("/api")
+      .then((response) => {
+        expect(typeof response).toBe("object");
+      });
+  });
+  test("responds with an object containing a property for each request", () => {
+    return request(app)
+      .get("/api")
+      .then((response) => {
+        const { endpoints } = response.body;
+        expect(endpoints.hasOwnProperty("GET /api")).toBe(true);
+        expect(endpoints.hasOwnProperty("GET /api/topics")).toBe(true);
+        expect(endpoints.hasOwnProperty("GET /api/articles")).toBe(true);
+      });
+  });
+  test("responds with a correct object containing the endpoint objects", () => {
+    return request(app)
+      .get("/api")
+      .then((response) => {
+        const { endpoints } = response.body;
+        const expected = {
+          "GET /api": {
+            description:
+              "serves up a json representation of all the available endpoints of the api",
+            requestFormat: null,
+          },
+          "GET /api/topics": {
+            description: "serves an array of all topics",
+            queries: [],
+            exampleResponse: {
+              topics: [{ slug: "football", description: "Footie!" }],
+            },
+            requestFormat: null,
+          },
+          "GET /api/articles": {
+            description: "serves an array of all articles",
+            queries: ["author", "topic", "sort_by", "order"],
+            exampleResponse: {
+              articles: [
+                {
+                  title: "Seafood substitutions are increasing",
+                  topic: "cooking",
+                  author: "weegembump",
+                  body: "Text from the article..",
+                  created_at: "2018-05-30T15:59:13.341Z",
+                  votes: 0,
+                  comment_count: 6,
+                },
+              ],
+            },
+            requestFormat: null,
+          },
+        };
+
+        expect(endpoints).toEqual(expected);
       });
   });
 });

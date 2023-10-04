2049e5722e11be3eec4b03425865ccdd61950d34
Brums16 <chrisbrummitt16@hotmail.co.uk>
2023-10-04 11:10:29 +0100

    task 9 on task 8 branch

--- models/topics-model.js
+++ models/topics-model.js
@@ -101,3 +101,18 @@ exports.updateArticle = async (inc_votes, id) => {
     });
   return rows[0];
 };
+
+exports.deleteComment = async (id) => {
+  const { rows } = await db.query(
+    `
+      DELETE FROM comments WHERE comment_id = $1 RETURNING *
+     `,
+    [id]
+  );
+  if (rows.length === 0) {
+    return Promise.reject({
+      status: 404,
+      msg: `No comment found for comment_id ${id}`,
+    });
+  }
+};

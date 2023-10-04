d43a88d90a26c0a3d7a1c706a1031a10ecbd4ce1
Brums16 <chrisbrummitt16@hotmail.co.uk>
2023-10-04 15:21:26 +0100

    task 11

--- controllers/get-articles.js
+++ controllers/get-articles.js
@@ -1,10 +1,13 @@
 const { fetchArticles } = require("../models/fetch-articles");
 
 exports.getArticles = async (req, res, next) => {
+  const { topic } = req.query;
+
   try {
-    const foundArticles = await fetchArticles();
+    const foundArticles = await fetchArticles(topic);
     return res.send({ articles: foundArticles });
   } catch (err) {
+    console.log(err, "err in controller");
     next(err);
   }
 };
--- models/fetch-articles.js
+++ models/fetch-articles.js
@@ -1,14 +1,30 @@
 const db = require("../db/connection");
+const format = require("pg-format");
 
 exports.fetchArticles = async (topic) => {
-  let queryStr = `
-    SELECT articles.article_id, title, topic, articles.author, articles.created_at,
-    articles.votes, article_img_url, CAST(COUNT(comment_id) AS int) AS comment_count
-    FROM articles 
-    LEFT JOIN comments ON comments.article_id = articles.article_id
-    GROUP BY articles.article_id
-    ORDER BY created_at DESC
-   `;
-  const { rows } = await db.query(queryStr);
+  let topicString = "";
+  if (topic) {
+    topicString = `WHERE topic = '${topic}'`;
+  }
+  const queryString = format(
+    `
+  SELECT articles.article_id, title, topic, articles.author, articles.created_at,
+  articles.votes, article_img_url, CAST(COUNT(comment_id) AS int) AS comment_count
+  FROM articles 
+  LEFT JOIN comments ON comments.article_id = articles.article_id
+  %s
+  GROUP BY articles.article_id
+  ORDER BY created_at DESC
+ `,
+    topicString
+  );
+
+  const { rows } = await db.query(queryString);
+  if (rows.length === 0) {
+    return Promise.reject({
+      status: 404,
+      msg: `No articles found for topic: ${topic}`,
+    });
+  }
   return rows;
 };

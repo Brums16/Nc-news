1e1e6ab7a15bea9f1f8d30c0c80a72ce6a15cdd8
Brums16 <chrisbrummitt16@hotmail.co.uk>
2023-10-05 10:32:48 +0100

    changes to connection.js

--- db/connection.js
+++ db/connection.js
@@ -16,6 +16,6 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-const db = new Pool();
+const db = new Pool(config);
 
 module.exports = db;
